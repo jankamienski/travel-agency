cd1d8f0231e540861571b6a81a7a5aec9726e8c0
jankamienski <kamienski.jan@gmail.com>
2021-02-13 18:51:34 +0100

    Add Tests for TripSummary  for checking and removing comments

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -6,36 +6,62 @@ describe ('Component TripSummary', () => {
   it ('should generate link to correct address', () => {
     const expectedLink = '/trip/abc';
     const expectedId = 'abc';
-    const expectedImg = 'image.jpg';
-    const expectedAltImg = 'text';
+    //const expectedImg = 'image.jpg';
+    //const expectedAltImg = 'text';
     const component = shallow (<TripSummary
       id={expectedId}
-      src={expectedImg}
-      alt={expectedAltImg}/>
+      //src={expectedImg}
+      //alt={expectedAltImg}
+      image= ''
+      name= ''
+      cost = ''
+      days = {1}
+      tags= {['tag1', 'tag2', 'tag3']}
+    />
     );
     expect (component.find('.link').prop('to')).toEqual(expectedLink);
-    expect (component.find('img').prop('src')).toEqual(expectedImg);
-    expect (component.find('img').prop('alt')).toEqual(expectedAltImg);
+    //expect (component.find('img').prop('src')).toEqual(expectedImg);
+    //expect (component.find('img').prop('alt')).toEqual(expectedAltImg);
   });
+
+  it ('has correct image src and alt', () => {
+    const expectedImg = 'image.jpg';
+    const expectedName = 'text';
+    const component = shallow (<TripSummary
+      image={expectedImg}
+      name={expectedName}
+      id = ''
+      cost =''
+      days = {1}
+      tags= {['tag1', 'tag2', 'tag3']}
+    />
+    );
+    expect (component.find('img').prop('src')).toEqual(expectedImg);
+    expect (component.find('img').prop('alt')).toEqual(expectedName);
+  });
+  
 
   it('shoud render correct props name, cost and days', () => {
     const expectedName = 'text';
-    const expectedCost = '$10';
+    const expectedCost = '$ 10';
     const expectedDays = 1;
-    const expectedId = 'abc';
-    const expectedImg = 'image.jpg';
+    //const expectedId = 'abc';
+    //const expectedImg = 'image.jpg';
     const component = shallow(<TripSummary
       name = {expectedName}
       cost = {expectedCost}
       days = {expectedDays}
-      id = {expectedId}
-      image = {expectedImg}/>
+      id = ''
+      image = ''
+      tags= {['tag1', 'tag2', 'tag3']}/>
     );
     expect (component.find('.title').text()).toEqual(expectedName);
-    expect (component.find('.details').text()).toEqual(expectedDays);
-    expect (component.find('.details').text()).toEqual(expectedCost); //jaki tag?
-    expect (component.find('.details').text()).toEqual(expectedId);  // jaki tag?
-    expect (component.find('.img').text()).toEqual(expectedImg);  
+    // (component.find('.details').text()).toEqual(expectedDays);
+    //expect (component.find('.details').text()).toEqual(expectedCost); //jaki tag?
+    //expect (component.find('.details').text()).toEqual(expectedId);  // jaki tag?
+    //expect (component.find('.img').text()).toEqual(expectedImg);  
+    expect (component.find('.details span').first().text()).toEqual(`${expectedDays} days`);
+    expect (component.find('.details span').last().text()).toEqual(`from ${expectedCost}`);
   });
 
   it('should throw error without required props', () => {
@@ -45,7 +71,12 @@ describe ('Component TripSummary', () => {
   it('should render tags in correct sequence', () => {
     const expectedTags = ['tag1', 'tag2', 'tag3'];
     const component = shallow(<TripSummary
-      tags= {expectedTags}/>
+      tags= {expectedTags}
+      name = ''
+      cost = ''
+      days = {1}
+      id = ''
+      image = ''/>
     );
     expect(component.find('.tags span').at(0).text()).toEqual(expectedTags[0]);
     expect(component.find('.tags span').at(1).text()).toEqual(expectedTags[1]);
@@ -55,7 +86,12 @@ describe ('Component TripSummary', () => {
   it('should not render span if the tags is false', () => {
     const expectedTags = [];
     const component = shallow(<TripSummary
-      tags={expectedTags}/>
+      tags={expectedTags}
+      name = ''
+      cost = ''
+      days = {1}
+      id = ''
+      image = ''/>
     );
     expect(component.hasClass('tags')).toBe(false);
   });

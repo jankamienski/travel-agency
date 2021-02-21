64e2ff3afcab09f021c32258d26ec119769439a6
jankamienski <kamienski.jan@gmail.com>
2021-02-21 17:43:15 +0100

    Add TDD test 2 rendering title and description

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import styles from './HappyHourAd.scss';
 
 class HappyHourAd extends React.Component {
   render() {
-    return {
-
-    };
+    return (
+      <div className ={styles.component}>
+        <h3 className ={styles.title}>Happy Hour</h3>
+        <div className={styles.promoDescription}></div>
+      </div>
+    );
   }
 }
 

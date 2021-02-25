5d31e8ba87494977e1b8b22f03ef92c786524e92
jankamienski <kamienski.jan@gmail.com>
2021-02-25 22:21:12 +0100

    Update integration tests for HappyHourAd

---
+++ src/components/features/daysToSummer/daysToSummer.js
@@ -0,0 +1,26 @@
+/*import React from 'react';
+import styles from './DaysToSummer.scss';
+
+class DaysToSummer extends React.Component {
+
+
+
+  render() {
+    return (
+      <div className={styles.component}>
+        <h3 className={styles.daysDescription}>X days to summer
+        
+        </h3>
+      </div>
+    );
+  }
+
+  //const currentDate = new Date();
+
+  // if dsy to sumemr = 1 
+  // wyswietl one day top sumemr
+  // else  jesli days to sumemr  < 0 to  zwracam  ''
+  // days to sumemr  ====
+}
+
+export default DaysToSummer;*/
---
+++ src/components/features/daysToSummer/daysToSummer.test.js
@@ -0,0 +1,34 @@
+/*import React from 'react';
+import {shallow} from 'enzyme';
+import DaysToSummer from './DaysToSummer';
+
+
+const select = {
+  daysDescription: '.daysDescription',
+};
+
+const mockProps = {
+  daysDescription: 'XXX days to summer',
+};
+
+describe('Component DaysToSummer', () => {
+  it('should render without crashing', () => {
+    const component = shallow (<DaysToSummer />);
+    expect(component).toBeTruthy();
+  });
+
+  it('should render description', () => {
+    const component = shallow (<DaysToSummer />);
+    expect(component.exists(select.daysDescription)).toEqual(true);
+  });
+
+  //czy ma poprawne klasy names
+  // shold render headings
+  
+
+  // check description at date  czy sumemr time is = ...xxx
+  // czy jak pdoam date  xx  czy dajac iles to bede mia ltyle days to summer
+
+});
+
+*/
